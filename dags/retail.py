b647068f30fc52daff83fc302b1817e0471a16ea
Alain Ahebie <alainahebie@gmail.com>
2024-01-10 00:20:53 +0100

    ADD RETAIL DAG

---
+++ dags/retail.py
@@ -0,0 +1,34 @@
+from airflow.decorators import dag, task
+from datetime import datetime
+
+from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyDatasetOperator
+
+from astro import sql as aql
+from astro.files import File
+from astro.sql.table import Table, Metadata
+from astro.constants import FileType
+
+@dag(
+    start_date=datetime(2024, 1, 1),
+    schedule=None,
+    catchup=False,
+    tags=['retail'],
+)
+
+def retail():
+    upload_csv_to_gcs = LocalFilesystemToGCSOperator(
+        task_id='upload_csv_to_gcs',
+        src='include/dataset/online_retail.csv',
+        dst='raw/online_retail.csv',
+        bucket='alain_online_retail',
+        gcp_conn_id='gcp',
+        mime_type='text/csv',
+    )
+        
+    create_retail_dataset = BigQueryCreateEmptyDatasetOperator(
+        task_id='create_retail_dataset',
+        dataset_id='retail',
+        gcp_conn_id='gcp',
+    )
+retail()
