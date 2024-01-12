2e5f231f32e756b342a3103dd3534609079530fb
Alain Ahebie <alainahebie@gmail.com>
2024-01-12 12:44:32 +0100

    ADD AND TEST DATA QUALITY TASK

--- dags/retail.py
+++ dags/retail.py
@@ -46,4 +46,11 @@ def retail():
         ),
         use_native_support=False,
     )
+    
+    @task.external_python(python='/mnt/c/Users/AHEBIE/Documents/GHUB/AIRFOW_SODA/soda_venv/bin/python')
+    def check_load(scan_name='check_load', checks_subpath='sources'):
+        from include.soda.check_function import check
+
+        return check(scan_name, checks_subpath)  
+
 retail()
