217bf47f6d75ee4e99a3464443646ba9ac446d31
Alain Ahebie <alainahebie@gmail.com>
2024-01-14 01:34:33 +0100

    Data quality check for report data

--- dags/retail.py
+++ dags/retail.py
@@ -4,6 +4,8 @@ from datetime import datetime
 from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
 from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyDatasetOperator
 
+from airflow.models.baseoperator import chain
+
 from astro import sql as aql
 from astro.files import File
 from astro.sql.table import Table, Metadata
@@ -59,7 +61,7 @@ def retail():
 
         return check(scan_name, checks_subpath)  
 
-    check_load()
+
     
     transform = DbtTaskGroup(
         group_id='transform',
@@ -71,4 +73,43 @@ def retail():
         )
     )
     
+    
+    @task.external_python(python='/usr/local/airflow/soda_venv/bin/python')
+    def check_transform(scan_name='check_transform', checks_subpath='transform'):
+        from include.soda.check_function import check
+
+        return check(scan_name, checks_subpath)
+
+
+
+    
+    
+    report = DbtTaskGroup(
+        group_id='report',
+        project_config=DBT_PROJECT_CONFIG,
+        profile_config=DBT_CONFIG,
+        render_config=RenderConfig(
+            load_method=LoadMode.DBT_LS,
+            select=['path:models/report']
+        )
+    )
+    
+    @task.external_python(python='/usr/local/airflow/soda_venv/bin/python')
+    def check_report(scan_name='check_report', checks_subpath='report'):
+        from include.soda.check_function import check
+
+        return check(scan_name, checks_subpath)
+    
+
+    
+    chain(
+        upload_csv_to_gcs,
+        create_retail_dataset,
+        gcs_to_raw,
+        check_load(),
+        transform,
+        check_transform(),
+        report,
+        check_report(),
+    )
 retail()
